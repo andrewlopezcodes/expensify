033a81155a28232847976da87d316e7b564cd90c
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-26 15:21:19 -0400

    setup url routes, installed react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
@@ -27,14 +27,22 @@ const HelpPage =()=>(
     This is from my help component
   </div>
 );
+
+const NotFoundPage =()=>(
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} exact={true}/>
         <Route path="/edit" component={EditExpensePage} exact={true}/>
         <Route path="/help" component={HelpPage} exact={true}/>
-    </div>
+        <Route component={NotFoundPage} />
+    </Switch>
 
   </BrowserRouter>
 )
