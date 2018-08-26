f71a7e4e45ecb001d6e131f6b228fca50c5354ee
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-26 15:09:51 -0400

    removed files, created boilerplate, start setting up routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
+const ExpenseDashboardPage =()=>(
+  <div>
+    This is from my dashboard component
+  </div>
+);
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
+const AddExpensePage =()=>(
+  <div>
+    This is from my expense component
+  </div>
+);
+
+const EditExpensePage =()=>(
+  <div>
+    This is from my edit component
+  </div>
+);
+
+const HelpPage =()=>(
+  <div>
+    This is from my help component
+  </div>
+);
+const routes = (
+  <BrowserRouter>
+    <div>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage} exact={true}/>
+        <Route path="/edit" component={EditExpensePage} exact={true}/>
+        <Route path="/help" component={HelpPage} exact={true}/>
+    </div>
+
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
--- src/components/Signature.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Signature =(props)=>(
-    <div>
-      <p className="signature">Created by andrewlopezcodes</p>
-    </div>
-  );
-
-export default Signature;
