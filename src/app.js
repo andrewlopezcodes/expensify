323ab4c07884a468419a7cc48919c7b5152eefbc
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-27 00:18:24 -0400

    created components files installed react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,50 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-const ExpenseDashboardPage =()=>(
-  <div>
-    This is from my dashboard component
-  </div>
-);
 
-const AddExpensePage =()=>(
-  <div>
-    This is from my expense component
-  </div>
-);
-
-const EditExpensePage =()=>(
-  <div>
-    This is from my edit component
-  </div>
-);
-
-const HelpPage =()=>(
-  <div>
-    This is from my help component
-  </div>
-);
-
-const NotFoundPage =()=>(
-  <div>
-    404!
-  </div>
-);
-
-const routes = (
-  <BrowserRouter>
-    <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} exact={true}/>
-        <Route path="/edit" component={EditExpensePage} exact={true}/>
-        <Route path="/help" component={HelpPage} exact={true}/>
-        <Route component={NotFoundPage} />
-    </Switch>
-
-  </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
