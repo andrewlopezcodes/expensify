d99ad4230dbe8d3e6e57b8e1c3bf99a3b7eb22fb
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-09-15 09:48:19 -0400

    refactored AddExpensePage, EditExpensePage to class components finished EditExpensePage test, refactored AddExpensePage test

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,9 +3,26 @@ import ExpenseForm from './ExpenseForm';
 import {connect} from 'react-redux';
 import {addExpense} from '../actions/expenses';
 
+// export class AddExpensePage extends React.Component {
+//   onSubmit=(expense)=>{
+//     this.props.onSubmit(expense);
+//     this.props.history.push('/');
+//   };
+//   render(){
+//     return(
+//       <div>
+//         <h1>Add Expense</h1>
+//       <ExpenseForm
+//         onSubmit={this.onSubmit}
+//       />
+//       </div>
+//     );
+//   }
+// }
+
 export class AddExpensePage extends React.Component {
   onSubmit=(expense)=>{
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push('/');
   };
   render(){
@@ -20,8 +37,20 @@ export class AddExpensePage extends React.Component {
   }
 }
 
+
+// const AddExpensePage =(props)=>(
+//   <div>
+//     <h1>Add Expense</h1>
+//     <ExpenseForm
+//       onSubmit={(expense)=>{
+//         props.onSubmit(expense);
+//         props.history.push('/');
+//       }}/>
+//   </div>
+// );
+
 const mapDispatchToProps = (dispatch)=>({
-  onSubmit: (expense) => dispatch(addExpense(expense))
+  addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 
--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,11 +3,11 @@ import {shallow} from 'enzyme';
 import {AddExpensePage} from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let onSubmit, history, wrapper;
-beforeEact(()=>{
-   onSubmit = jest.fn();
+let addExpense, history, wrapper;
+beforeEach(()=>{
+   addExpense = jest.fn();
    history = { push: jest.fn()};
-   wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+   wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
 });
 
 
@@ -19,5 +19,5 @@ test('should render AddExpensePage correctly', () =>{
 test('should handle onSubmit', () =>{
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
